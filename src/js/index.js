4e559c88fb516f6e25e61de72918dfc39f8763e2
master <master2011zhao@gmail.com>
2020-03-01 16:49:33 +0800

    -

--- src/js/index.js
+++ src/js/index.js
@@ -26,15 +26,17 @@ const handleReadEvent = () => {
             path: folderPath.value + '/' + m,
             size: 0,
             status: 0,
-            date: ''
+            date: '',
+            isFile: false
           }
           arr.push(obj);
           try {
             const fileInfo = fs.statSync(obj.path);
             obj.size = fileInfo.size ? Math.round(fileInfo.size / 1024) + " KB" : 0;
             obj.date = fileInfo.birthtime ? new Date(fileInfo.birthtime).format('yyyy-MM-dd hh:mm:ss') : '';
+            obj.isFile = fileInfo.isFile();
           } catch (error) {}
-          console.log(fs.statSync(obj.path).birthtime);
+          console.log(fs.statSync(obj.path).isFile());
           // str += `<div class="resultArea_item" path="${folderPath.value}/${m}">${m}</div>`
         });
         // 排序
@@ -42,20 +44,21 @@ const handleReadEvent = () => {
           return new Date(b.date).getTime() - new Date(a.date).getTime();
         })
         arr.forEach((n, i) => {
+          const fileTd = n.isFile ? `<td class="textCenter handleFilePreview" path="${n.path}">点击预览</td>` : `<td class="textCenter">--</td>`
           str += `<tr>
           <td class="textCenter">${i+1}</td>
           <td>${n.name}</td>
           <td>${n.date}</td>
           <td>${n.path}</td>
-          <td class="textCenter">${n.size}</td>
+          <td class="textCenter">${n.isFile ? n.size : '--'}</td>
           <td class="textCenter">${n.status == 0 ? "未上传" : "已上传"}</td>
-          <td class="textCenter handleFilePreview" path="${n.path}">点击预览</td>
+          ${fileTd}
           </tr>`
         });
       }
       resultDom.getElementsByTagName('tbody')[0].innerHTML = str;
     } catch (error) {
-      resultDom.getElementsByTagName('tbody')[0].innerHTML = error;
+      resultDom.getElementsByTagName('tbody')[0].innerHTML = `<tr><td class="textCenter error" colspan="7">${error}</td></tr>`;
     }
   }
 };
