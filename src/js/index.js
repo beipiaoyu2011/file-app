ca7aaa90eebca366539b10cbf87185ab8e9a89d8
master <master2011zhao@gmail.com>
2020-03-01 16:59:37 +0800

    status

--- src/js/index.js
+++ src/js/index.js
@@ -9,10 +9,20 @@ const defaultPath = localStorage.getItem('readPath');
 if (defaultPath) {
   folderPath.value = defaultPath;
 }
-const previewClick = url => window.open(url);
 // 处理文件
 const handleReadEvent = () => {
+
   if (folderPath && folderPath.value) {
+    // 判断是否是文件夹
+    try {
+      const inputPathInfo = fs.statSync(folderPath.value);
+      if (inputPathInfo.isFile()) {
+        alert("当前路径不是文件夹，请输入文件夹路径");
+        return;
+      }
+    } catch (error) {
+      resultDom.getElementsByTagName('tbody')[0].innerHTML = `<tr><td class="textCenter error" colspan="7">${error}</td></tr>`;
+    }
     try {
       root = fs.readdirSync(folderPath.value);
       localStorage.setItem('readPath', folderPath.value);
@@ -40,7 +50,7 @@ const handleReadEvent = () => {
           // str += `<div class="resultArea_item" path="${folderPath.value}/${m}">${m}</div>`
         });
         // 排序
-        arr.sort(function(a, b){
+        arr.sort(function (a, b) {
           return new Date(b.date).getTime() - new Date(a.date).getTime();
         })
         arr.forEach((n, i) => {
@@ -51,7 +61,7 @@ const handleReadEvent = () => {
           <td>${n.date}</td>
           <td>${n.path}</td>
           <td class="textCenter">${n.isFile ? n.size : '--'}</td>
-          <td class="textCenter">${n.status == 0 ? "未上传" : "已上传"}</td>
+          <td class="textCenter">${n.isFile ? (n.status == 0 ? "未上传" : "已上传") : '--'}</td>
           ${fileTd}
           </tr>`
         });
@@ -74,11 +84,11 @@ setInterval(() => {
   handleReadEvent();
 }, 60 * 1000);
 // 所有点击事件
-function handleAllEvent(){
+function handleAllEvent() {
   // 点击预览
-  document.querySelector('.resultArea').addEventListener('click', (e)=>{
+  document.querySelector('.resultArea').addEventListener('click', (e) => {
     const target = e.target;
-    if(target.classList.contains('handleFilePreview')){
+    if (target.classList.contains('handleFilePreview')) {
       const path = target.getAttribute('path');
       window.open(path);
     }
