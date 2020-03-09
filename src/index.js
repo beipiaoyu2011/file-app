409a5e9ed03a9e6cdf77123bb8b0e42d5c1d8d00
master <master2011zhao@gmail.com>
2020-03-09 15:23:27 +0800

    -

--- src/index.js
+++ src/index.js
@@ -1,69 +1,84 @@
 import {
-  app,
-  BrowserWindow,
-  Notification,
-  Menu
+	app,
+	BrowserWindow,
+	Notification,
+	Menu,
+	ipcMain
 } from 'electron';
+const exec = require('child_process').execFile;
+const path = require('path')
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
-  app.quit();
+	app.quit();
 }
+
+// 执行依赖 EXE
+const execFun = function () {
+	console.log("fun() start");
+	exec(path.resolve(__dirname, 'apowersoft-online-launcher.exe'), function (err, data) {
+		console.log(err)
+		console.log(data.toString());
+	});
+}
+
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
 
 const createWindow = () => {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
-    width: 1200,
-    height: 800,
-  });
+	// Create the browser window.
+	mainWindow = new BrowserWindow({
+		width: 1200,
+		height: 900,
+	});
 
-  // and load the index.html of the app.
-  // mainWindow.loadURL(`file://${__dirname}/index.html`);
-  mainWindow.loadURL(`file://${__dirname}/record.html`);
-  // mainWindow.loadURL(`http://192.168.6.196`);
+	// and load the index.html of the app.
+	// mainWindow.loadURL(`file://${__dirname}/index.html`);
+	mainWindow.loadURL(`file://${__dirname}/record.html`);
+	// mainWindow.loadURL(`http://192.168.6.196`);
 
+	// Open the DevTools.
+	// mainWindow.webContents.openDevTools();
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools();
+	// Emitted when the window is closed.
+	mainWindow.on('closed', () => {
+		// Dereference the window object, usually you would store windows
+		// in an array if your app supports multi windows, this is the time
+		// when you should delete the corresponding element.
+		mainWindow = null;
+	});
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-  });
+	// 当前的可执行文件所在目录
+	execFun();
 
-  // let myNotification = new Notification({
-  //   title: 'test',
-  //   body: 'hello'
-  // })
+	// let myNotification = new Notification({
+	//   title: 'test',
+	//   body: 'hello'
+	// })
 
-  // myNotification.show();
+	// myNotification.show();
 
-  // mac dock menu
-  // const dockMenu = Menu.buildFromTemplate([{
-  //     label: 'New Window',
-  //     click() {
-  //       console.log('New Window')
-  //     }
-  //   }, {
-  //     label: 'New Window with Settings',
-  //     submenu: [{
-  //         label: 'Basic'
-  //       },
-  //       {
-  //         label: 'Pro'
-  //       }
-  //     ]
-  //   }
-  // ])
+	// mac dock menu
+	// const dockMenu = Menu.buildFromTemplate([{
+	//     label: 'New Window',
+	//     click() {
+	//       console.log('New Window')
+	//     }
+	//   }, {
+	//     label: 'New Window with Settings',
+	//     submenu: [{
+	//         label: 'Basic'
+	//       },
+	//       {
+	//         label: 'Pro'
+	//       }
+	//     ]
+	//   }
+	// ])
 
-  // app.dock.setMenu(dockMenu)
+	// app.dock.setMenu(dockMenu)
 
 };
 
@@ -74,19 +89,19 @@ app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+	// On OS X it is common for applications and their menu bar
+	// to stay active until the user quits explicitly with Cmd + Q
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
 });
 
 app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow();
-  }
+	// On OS X it's common to re-create a window in the app when the
+	// dock icon is clicked and there are no other windows open.
+	if (mainWindow === null) {
+		createWindow();
+	}
 });
 
 // In this file you can include the rest of your app's specific main process
